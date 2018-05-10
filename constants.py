37b3d013d2e184e75ba6fb2bdf411971c69d972e
Fernando Hernandez <fernandoh41500@24dcaea7d349>
2018-05-10 15:56:13 +0000

    Switched to using Flask-Bootstrap

--- constants.py
+++ constants.py
@@ -1,37 +1,5 @@
-import course
 import song
-COURSES = [
-    course.Course(period=1,
-                  name='Intro to Computer Science',
-                  teacher_name='Ms. Lee',
-                  resource_name='repl.it',
-                  resource_url='https://repl.it/'),
-    course.Course(period=2,
-                  name='Italian 2',
-                  teacher_name='Ms. Franceschi',
-                  resource_name='Word Ref.',
-                  resource_url='http://www.wordreference.com/'),
-    course.Course(period=3,
-                  name='Algebra 2',
-                  teacher_name='Mr. Shelton',
-                  resource_name='Khan Academy',
-                  resource_url='https://www.khanacademy.org/math/algebra2'),
-    course.Course(period=4,
-                  name='Government',
-                  teacher_name='Mr. Capitan',
-                  resource_name='Amendments',
-                  resource_url='hrlibrary.umn.edu/education/all_amendments_usconst.htm'),
-    course.Course(period=5,
-                  name='ERWC',
-                  teacher_name='Ms. Dano',
-                  resource_name='Audio Book',
-                  resource_url='https://www.audiobooks.com/'),
-    course.Course(period=6,
-                  name='Biotech 1/2',
-                  teacher_name='Mr. Ng',
-                  resource_name='Wikipedia',
-                  resource_url='https://en.wikipedia.org/wiki/Biology'),
-]
+
 
 
 TOP_TEN_SONGS = [
@@ -64,13 +32,7 @@ TOP_TEN_SONGS = [
         youtube_url="https://www.youtube.com/watch?v=e73OTS_W3qw"),
     song.Song(title="Sacrifices",
         artist_name="Drake",
-        youtube_url="https://www.youtube.com/watch?v=2RtbGnpDa7E"),
-
-
-
-
-
-
+        youtube_url="https://www.youtube.com/watch?v=2RtbGnpDa7E")
 ]
 
 
--- flask_app.py
+++ flask_app.py
@@ -5,8 +5,35 @@ from flask import Flask
 from flask import render_template
 import constants
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+
+
+
 
 app = Flask(__name__)
+
+app.config.from_object('config.BaseConfig')
+db = SQLAlchemy(app)
+
+Bootstrap(app)
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    period = db.Column(db.Integer)
+    name = db.Column(db.String(80))
+    teacher_name = db.Column(db.String(80))
+    resource_name = db.Column(db.String(80))
+    resource_url = db.Column(db.String(300))
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    artist_name = db.Column(db.String(80))
+    youtube_url = db.Column(db.String(300))
+
+
 
 @app.route('/')
 def hello_world():
@@ -14,19 +41,18 @@ def hello_world():
 
 @app.route('/about_me')
 def about_me():
-    return render_template('about_me.html',
-                           courses=constants.COURSES)
+    return render_template('about_me.html')
 
 
 @app.route('/class_schedule')
 def class_schedule():
+    courses = Course.query.all()
     return render_template('class_schedule.html',
-                           courses=constants.COURSES)
+                           courses=courses)
 
 @app.route('/register')
 def register():
-    return render_template('register.html',
-                            courses=constants.COURSES)
+    return render_template('register.html')
 
 @app.route('/index')
 def homepage():
@@ -35,12 +61,13 @@ def homepage():
 
 @app.route('/top_ten_songs')
 def top_ten_songs():
-    return render_template('top_ten_songs.html', songs=constants.TOP_TEN_SONGS)
+    songs= Song.query.all()
+    return render_template('top_ten_songs.html',
+                            songs=songs)
 
 
-app.config.from_object('config.BaseConfig')
-db = SQLAlchemy(app)
-
+if __name__ == '__main__':
+    db.create_all()
 
 
 
